8aa1be1b88f6ab9b26bf37178cd67b558c786384
ezequiel.quse <ezequiel.quse@sismogames.com>
2022-06-07 20:42:11 -0300

    en clase

--- src/scenes/mainmenu.js
+++ src/scenes/mainmenu.js
@@ -1,3 +1,5 @@
+import Button from "../js/button.js";
+
 // Clase MainMenu, donde se crean los botones, el logo y el fondo del menú principal
 export class MainMenu extends Phaser.Scene {
   constructor() {
@@ -7,19 +9,13 @@ export class MainMenu extends Phaser.Scene {
 
   create() {
     // Fondo del menú principal
-    this.add
-      .image(
-        this.cameras.main.centerX,
-        this.cameras.main.centerY,
-        "mainmenu_bg"
-      )
-      .setScale(1.1);
+    this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'mainmenu_bg').setScale(1.1);
+   
 
-    // Logo de Phaser
-    this.add.image(
-      this.cameras.main.centerX,
-      this.cameras.main.centerY / 1.5,
-      "phaser_logo"
-    );
-  }
+    // Boton para comenzar a jugar
+    const boton = new Button(this.cameras.main.centerX, this.cameras.main.centerY + this.cameras.main.centerY/3, 'Play', this, () => {
+        // Instrucción para pasar a la escena Play
+        this.scene.start("Play");
+    });
+}
 }
